2eca21b28a279989e0cd50fa62e88d149aa1734b
Carson Weeks <CarsonWeeks@mail.com>
2022-04-14 13:05:37 -0700

    initial commit.

---
+++ frontend/src/main.js
@@ -0,0 +1,6 @@
+import Alpine from "alpinejs";
+import "./style.css";
+
+Alpine.start();
+
+console.log("Hello world!");
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}", "../devproject/templates/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
