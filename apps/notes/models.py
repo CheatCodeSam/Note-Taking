b8500f19e7a38629341fe225dec6965282fc9dbc
Carson Weeks <CarsonWeeks@mail.com>
2022-04-16 14:29:24 -0700

    Created Note model.

--- apps/notes/models.py
+++ apps/notes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+
+class Note(models.Model):
+    id = ShortUUIDField(
+        length=16,
+        max_length=40,
+        prefix="id_",
+        primary_key=True,
+    )
+    title = models.CharField(max_length=30, null=False, blank=True)
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.title
