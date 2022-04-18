b82dc9a607c8ff82ad74f4ad17ea504647d21601
Carson Weeks <CarsonWeeks@mail.com>
2022-04-18 13:31:26 -0700

    Added rendering of notes.

--- apps/notes/urls.py
+++ apps/notes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path("", index, name="home"),
+    path("<str:shortuuid>/", index, name="home"),
 ]
--- apps/notes/views.py
+++ apps/notes/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Note
 
 
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "apps/notes/index.html")
+def index(request, shortuuid):
+    note = get_object_or_404(Note, pk=shortuuid)
+    return render(request, "apps/notes/index.html", {"note": note})
