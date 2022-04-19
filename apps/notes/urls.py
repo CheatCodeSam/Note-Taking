bd6f57a6efa3c11b9168b03b7e380eb0b6e0d3f8
Carson Weeks <CarsonWeeks@mail.com>
2022-04-19 12:50:29 -0700

    Added NoteCreateView.

--- apps/notes/urls.py
+++ apps/notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import detail
+from .views import detail, NoteCreateView
 
 urlpatterns = [
-    path("<str:shortuuid>/", detail, name="detail"),
+    path("<str:shortuuid>/", detail, name="note-detail"),
+    path("", NoteCreateView.as_view(), name="note-create"),
 ]
--- apps/notes/views.py
+++ apps/notes/views.py
@@ -1,11 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import Note
-
-
+from django.views.generic.edit import CreateView
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def detail(request, shortuuid):
     note = get_object_or_404(Note, pk=shortuuid)
     return render(request, "apps/notes/detail.html", {"note": note})
+
+
+class NoteCreateView(CreateView):
+    template_name = "apps/notes/form.html"
+    model = Note
+    fields = ["title", "content"]
+
+    def get_success_url(self):
+        return reverse("apps:note-detail", kwargs={"shortuuid": self.object.id})
